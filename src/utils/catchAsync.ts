d61a50b613133b2fad828f215ef655138577ec73
Victor Oluwayemi <oluwayemivictor15@gmail.com>
2026-04-27 14:00:13 +0100

    feat(format): format file with prettier

--- src/utils/catchAsync.ts
+++ src/utils/catchAsync.ts
@@ -1,7 +1,11 @@
 import { Request, Response, NextFunction } from 'express';
 
 export const catchAsync = (
-  fn: (req: Request, res: Response, next: NextFunction) => Promise<void>,
+  fn: (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ) => Promise<unknown>,
 ) => {
   return (req: Request, res: Response, next: NextFunction) => {
     Promise.resolve(fn(req, res, next)).catch(next);
