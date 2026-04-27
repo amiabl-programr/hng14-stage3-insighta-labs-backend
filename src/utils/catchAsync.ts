426cac63c69f2b889716f65fbe5ff27ac123c7ca
Victor Oluwayemi <oluwayemivictor15@gmail.com>
2026-04-27 13:52:59 +0100

    refactor: remove implicit type definition

--- src/utils/catchAsync.ts
+++ src/utils/catchAsync.ts
@@ -1,6 +1,8 @@
 import { Request, Response, NextFunction } from 'express';
 
-export const catchAsync = (fn: Function) => {
+export const catchAsync = (
+  fn: (req: Request, res: Response, next: NextFunction) => Promise<void>,
+) => {
   return (req: Request, res: Response, next: NextFunction) => {
     Promise.resolve(fn(req, res, next)).catch(next);
   };
