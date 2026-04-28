a9f2454ddde8d8032002c5f569ccbd0e8e433726
Victor Oluwayemi <oluwayemivictor15@gmail.com>
2026-04-28 13:08:01 +0100

    style: format file with prettier

--- src/test/app.test.ts
+++ src/test/app.test.ts
@@ -1,15 +1,15 @@
-import request from "supertest";
-import app from "../app.js";
-import { describe, expect, it, afterAll } from "@jest/globals";
-import { prisma } from "../lib/prisma.js";
+import request from 'supertest';
+import app from '../app.js';
+import { describe, expect, it, afterAll } from '@jest/globals';
+import { prisma } from '../lib/prisma.js';
 
-describe("GET /api/profiles", () => {
-  it("should return code 200 for valid profiles", async () => {
+describe('GET /api/profiles', () => {
+  it('should return code 200 for valid profiles', async () => {
     const response = await request(app)
-      .get("/api/profiles?search=victor")
+      .get('/api/profiles?search=victor')
       .expect(200);
 
-    expect(response.body.status).toBe("success");
+    expect(response.body.status).toBe('success');
   });
 
   afterAll(async () => {
