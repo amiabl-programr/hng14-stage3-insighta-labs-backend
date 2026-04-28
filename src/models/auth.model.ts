6b90b30809f50d57771ab8365de660c0e20cc5ad
Victor Oluwayemi <oluwayemivictor15@gmail.com>
2026-04-28 12:19:40 +0100

    refactor(style): format code with prettier

--- src/models/auth.model.ts
+++ src/models/auth.model.ts
@@ -2,76 +2,74 @@ import { prisma } from '../lib/prisma.js';
 import { GithubUserData } from '../services/auth.service.js';
 
 export async function saveUser(user: GithubUserData) {
+  const githubId = Number(user.github_id);
 
-    const githubId = Number(user.github_id);
+  if (!githubId) {
+    throw new Error('Invalid GitHub ID');
+  }
 
-    if (!githubId) {
-      throw new Error('Invalid GitHub ID');
-    }
+  // 1. Find existing account
+  const existingAccount = await prisma.account.findUnique({
+    where: {
+      provider_providerAccountId: {
+        provider: 'github',
+        providerAccountId: githubId,
+      },
+    },
+    include: { user: true },
+  });
 
-    // 1. Find existing account
-    const existingAccount = await prisma.account.findUnique({
+  if (existingAccount) {
+    //   Update existing user + tokens
+    const updated = await prisma.account.update({
       where: {
         provider_providerAccountId: {
           provider: 'github',
           providerAccountId: githubId,
         },
       },
-      include: { user: true },
-    });
-
-    if (existingAccount) {
-      //   Update existing user + tokens
-      const updated = await prisma.account.update({
-        where: {
-          provider_providerAccountId: {
-            provider: 'github',
-            providerAccountId: githubId,
-          },
-        },
-        data: {
-          access_token: user?.access_token,
-          refresh_token: user?.refresh_token,
-          expires_at: user?.expires_at,
-
-          user: {
-            update: {
-              username: user.username,
-              email: user.email ?? undefined,
-              avatar_url: user.avatar_url,
-            },
-          },
-        },
-        include: { user: true },
-      });
-
-      return updated.user;
-    }
-
-    // Create new user + account
-    const newAccount = await prisma.account.create({
       data: {
-        provider: 'github',
-        providerAccountId: githubId,
-
         access_token: user?.access_token,
         refresh_token: user?.refresh_token,
         expires_at: user?.expires_at,
 
         user: {
-          create: {
+          update: {
             username: user.username,
-            github_id: githubId, // fix  this repitition later
             email: user.email ?? undefined,
             avatar_url: user.avatar_url,
-            role: 'ANALYST',
-            is_active: true,
           },
         },
       },
       include: { user: true },
     });
 
-    return newAccount.user;
+    return updated.user;
+  }
 
+  // Create new user + account
+  const newAccount = await prisma.account.create({
+    data: {
+      provider: 'github',
+      providerAccountId: githubId,
+
+      access_token: user?.access_token,
+      refresh_token: user?.refresh_token,
+      expires_at: user?.expires_at,
+
+      user: {
+        create: {
+          username: user.username,
+          github_id: githubId, // fix  this repitition later
+          email: user.email ?? undefined,
+          avatar_url: user.avatar_url,
+          role: 'ANALYST',
+          is_active: true,
+        },
+      },
+    },
+    include: { user: true },
+  });
+
+  return newAccount.user;
 }
