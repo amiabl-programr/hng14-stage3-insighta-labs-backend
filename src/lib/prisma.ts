2c6991926945f75e6cc6968610f51ce20beadc2f
Victor Oluwayemi <oluwayemivictor15@gmail.com>
2026-05-08 09:47:04 +0100

    fix: refactor Prisma adapter initialization to use connection pool

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,13 +1,14 @@
 import 'dotenv/config';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '../generated/prisma/client.js';
+import pg from 'pg';
 
-const connectionString = `${process.env.DATABASE_URL}`;
-
-const adapter = new PrismaPg({
-  connectionString,
+const pool = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
   max: 20,
 });
+
+const adapter = new PrismaPg(pool);
 const prisma = new PrismaClient({ adapter });
 
 export { prisma };
